c068cfd5d39392bc571e6c73ef15862234b43723
simeonmarkov <simeon.markov1994@gmail.com>
2018-06-01 23:30:15 +0300

    insert right/wrong text for each question after test is finished

--- app/scripts/finish_test.js
+++ app/scripts/finish_test.js
@@ -1,35 +1,26 @@
+function finishTest() {
+    if (confirm("Приключване на теста?")) {
+        stopTime();
+        let testDiv = document.getElementById("testControl");
+        testDiv.parentNode.removeChild(testDiv);
+        showAnswers();
+    }
+}
+
 function showAnswers() {
     $(document).ready(function () {
         $.getJSON('./resources/answers.json', function (data) {
             $.each(data, function (k, v) {
                 var answers = document.getElementsByName(k);
                 for (var i = 0, length = answers.length; i < length; i++) {
-                    if (answers[i].value === v) {
-                        answers[i].setAttribute("class", "right");
-                    } else if (answers[i].checked) {
-                        answers[i].setAttribute("class", "wrong");
+                    if(answers[i].value === v) {
+                        answers[i].nextSibling.nodeValue += (' \" <div id="wrong">Appended right</div>.\"');
+                    } else if(answers[i].checked){
+                        answers[i].nextSibling.nodeValue += (' \" <div id="wrong">Appended wrong</div>.\"');
                     }
                 }
             });
         });
     });
-    return 4;
-}
 
-function finishTest() {
-    if (confirm("Сигурен ли сте, че искате да приключите теста?")) {
-        stopTime();
-        let testDiv = document.getElementById("testControl");
-        testDiv.parentNode.removeChild(testDiv);
-        var answersRevealed = showAnswers();
-        console.log(answersRevealed);
-        let rightAnswers = document.getElementsByClassName("right");
-        for (let i = 0; i < rightAnswers.length; i++) {
-            $(rightAnswers[i].className).append('<img src="../resources/ball.png" width="30" height="30"/>');
-        }
-        let wrongAnswers = document.getElementsByClassName("wrong");
-        for (let i = 0; i < wrongAnswers.length; i++) {
-            wrongAnswers[i].after('<img src="../resources/ball.png" width="30" height="30"/>');
-        }
-    }
 }
