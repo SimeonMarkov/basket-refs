17ec88f133b2fc86025167ddc71d71c2a38a678b
simeonmarkov <simeon.markov1994@gmail.com>
2018-06-02 01:31:15 +0300

    added separate answer jsons for different tests

--- app/scripts/finish_test.js
+++ app/scripts/finish_test.js
@@ -1,20 +1,20 @@
-function finishTest() {
+function finishTest(test) {
     if (confirm("Приключване на теста?")) {
         stopTime();
         let testDiv = document.getElementById("testControl");
         testDiv.parentNode.removeChild(testDiv);
-        showAnswers();
+        showAnswers(test);
     }
 }
 
-function showAnswers() {
+function showAnswers(test) {
     $(document).ready(function () {
-        $.getJSON('./resources/answers.json', function (data) {
+        $.getJSON(test, function (data) {
             $.each(data, function (k, v) {
                 var answers = document.getElementsByName(k);
                 for (var i = 0, length = answers.length; i < length; i++) {
                     if(answers[i].value === v) {
-                        answers[i].nextSibling.nodeValue += (' \" <div id="wrong">Appended right</div>.\"');
+                        answers[i].nextSibling.nodeValue += (' \" <div id="right">Appended right</div>.\"');
                     } else if(answers[i].checked){
                         answers[i].nextSibling.nodeValue += (' \" <div id="wrong">Appended wrong</div>.\"');
                     }
