aeea028bdc400a3680924e3eaf723f7dca799ea6
KrumTy <zarichepower@gmail.com>
2018-06-02 10:43:41 +0300

    add toggleble test data

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,15 +1,48 @@
 (function () {
-    function loadUser() {
-        var data = [{
-            name: 'Krum'
-        }, {
-            name: 'Simo'
-        }]
+    function renderData(data) {
         var template = $('#data-list').html();
         Mustache.parse(template);   // optional, speeds up future uses
-        var rendered = Mustache.render(template, {data: data});
+        var rendered = Mustache.render(template, { data: data });
         $('#data-list').html(rendered);
-    }
-    
-    loadUser();
+    };
+
+    function getData(params, cb) {
+        var data = [{
+            name: 'Правило 1',
+            data: [{
+                name: 'Чл. 1',
+                data: [{
+                    name: 'A paragraph of text with an unassigned link.'
+                }]
+            }, {
+                name: 'A second row of text with a web link'
+            }]
+        }, {
+            name: 'Правило 2 ',
+            data: [{
+                name: 'Чл. 2',
+                data: [{
+                    name: 'A paragraph of text with an unassigned link.'
+                }, {
+                    name: 'A second row of text with a web link'
+                }]
+            }, {
+                name: 'Чл. 3',
+                data: [{
+                    name: 'A paragraph of text with an unassigned link.'
+                }]
+            }, {
+                name: 'A second row of text with a web link'
+            }]
+        }];
+
+        cb(data);
+    };
+
+    getData({}, renderData);
+
+    $('.data-list-header').click(function () {
+        console.log('click');
+        $(this).next().toggleClass('hidden');
+    });
 })();
