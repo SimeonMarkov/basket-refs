fa76375425298fb9f9c98492f0849ae62b88a289
KrumTy <zarichepower@gmail.com>
2018-06-02 11:07:18 +0300

    add secretar scripts/data

---
+++ app/scripts/load-secretar-data.js
@@ -0,0 +1,69 @@
+(function () {
+    function renderData(data) {
+        var template = $('#data-list').html();
+        Mustache.parse(template);   // optional, speeds up future uses
+        var rendered = Mustache.render(template, { data: data });
+        $('#data-list').html(rendered);
+    };
+
+    function getData(params, cb) {
+        var data = [{
+            name: 'Правило 1',
+            data: [{
+                name: 'Чл. 1',
+                data: [{
+                    name: 'A paragraph of text with an unassigned link.'
+                }]
+            }, {
+                name: 'A second row of text with a web link'
+            }]
+        }, {
+            name: 'Правило 2 ',
+            data: [{
+                name: 'Чл. 2',
+                data: [{
+                    name: 'A paragraph of text with an unassigned link.'
+                }, {
+                    name: 'A second row of text with a web link'
+                }]
+            }, {
+                name: 'Чл. 3',
+                data: [{
+                    name: 'A paragraph of text with an unassigned link.'
+                }]
+            }, {
+                name: 'A second row of text with a web link'
+            }]
+        }, {
+            name: 'Правило 3 ',
+            data: [{
+                name: 'Чл. 4',
+                data: [{
+                    name: 'A paragraph of text with an unassigned link.'
+                }, {
+                    name: 'A second row of text with a web link'
+                }]
+            }, {
+                name: 'Чл. 5',
+                data: [{
+                    name: 'A paragraph of text with an unassigned link.'
+                }]
+            }, {
+                name: 'A second row of text with a web link'
+            }]
+        }];
+
+        cb(data);
+    };
+
+    getData({}, renderData);
+
+    $('.data-list-header').click(function () {
+        $(this).next().toggleClass('hidden');
+    });
+
+    $('#toggle-btn').click(function (ev) {
+        ev.preventDefault()
+        $('.data-list-items').toggleClass('hidden');
+    });
+})();
