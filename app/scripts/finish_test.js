e131556e4a2bfee1e17e4b697eaad47fbf81b88a
simeonmarkov <simeon.markov1994@gmail.com>
2018-06-03 10:40:56 +0300

    added correctlyAnswered/numberOfQuestions feedback

--- app/scripts/finish_test.js
+++ app/scripts/finish_test.js
@@ -8,18 +8,31 @@ function finishTest(test) {
 }
 
 function showAnswers(test) {
+    var answers;
+    var correctlyAnsweredCount = 0;
+    var questionsNumber = 0;
     $(document).ready(function () {
         $.getJSON(test, function (data) {
             $.each(data, function (k, v) {
-                var answers = document.getElementsByName(k);
+                answers = document.getElementsByName(k);
+                questionsNumber++;
                 for (var i = 0, length = answers.length; i < length; i++) {
                     if(answers[i].value === v) {
                         answers[i].nextSibling.nodeValue += (' \" <div id="right">Appended right</div>.\"');
+                        if(answers[i].checked) {
+                            correctlyAnsweredCount++;
+                        }
                     } else if(answers[i].checked){
                         answers[i].nextSibling.nodeValue += (' \" <div id="wrong">Appended wrong</div>.\"');
                     }
                 }
-            });
+                if(correctlyAnsweredCount/questionsNumber >= 0.5) {
+                    $('#test_result').css('color', 'mediumspringgreen');
+                } else {
+                    $('#test_result').css('color', 'red');
+                }
+                document.getElementById("test_result").innerText = "Оценка:" +  correctlyAnsweredCount + "/" + questionsNumber;
+            })
         });
     });
 
